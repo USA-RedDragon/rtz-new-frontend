34d992a45d425bfc5b41fef306a26d94c3c293bf
Jacob McSwain <jacob@mcswain.dev>
2024-07-22 12:00:17 -0500

    config: set to my API

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,2 +1,2 @@
-export const ATHENA_URL = 'https://athena.comma.ai'
-export const BASE_URL = 'https://api.comma.ai'
+export const ATHENA_URL = 'https://openpilot.jacob.network'
+export const BASE_URL = 'https://openpilot.jacob.network'
