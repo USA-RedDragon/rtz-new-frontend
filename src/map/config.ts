6dfc589db5c2baf2ac1fae5641cda795ba35c8e7
Jacob McSwain <jacob@mcswain.dev>
2024-07-22 13:51:03 -0500

    mapbox: swap to my styles

--- src/map/config.ts
+++ src/map/config.ts
@@ -1,7 +1,7 @@
-export const MAPBOX_USERNAME = 'commaai'
+export const MAPBOX_USERNAME = 'usa-reddragon'
 
-export const MAPBOX_LIGHT_STYLE_ID = 'clcl7mnu2000214s2zgcdly6e'
-export const MAPBOX_DARK_STYLE_ID = 'clcgvbi4f000q15t6o2s8gys3'
+export const MAPBOX_LIGHT_STYLE_ID = 'clyxccwdz00tx01p90qa01yni'
+export const MAPBOX_DARK_STYLE_ID = 'clyxcc7mc00st01nxh4xp4oy7'
 
 // TODO: validate env or throw error
 export const MAPBOX_TOKEN = import.meta.env.VITE_MAPBOX_TOKEN as string
